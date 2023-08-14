66e5fec8e12f880d51c571141ea6a62f52aedd8d
Matheus Baron <matheusbaron10@gmail.com>
2023-08-14 20:31:21 -0300

    feat: create header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from "next/app"
 import { GlobalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+
 GlobalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
