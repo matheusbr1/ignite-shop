3be855aa9aa36528244512211edcec61c4f6eed3
Matheus Baron <matheusbaron10@gmail.com>
2023-08-15 12:50:05 -0300

    feat: add keen-slider lib to create carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,20 @@ import camiseta1 from '../assets/1.png'
 import camiseta2 from '../assets/2.png'
 import camiseta3 from '../assets/3.png'
 
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider" >
+      <Product className='keen-slider__slide' >
         <Image src={camiseta1} width={520} height={480} alt='camiseta' />
 
         <footer>
@@ -17,7 +27,34 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide' >
+        <Image src={camiseta1} width={520} height={480} alt='camiseta' />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide' >
+        <Image src={camiseta1} width={520} height={480} alt='camiseta' />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide' >
+        <Image src={camiseta1} width={520} height={480} alt='camiseta' />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide' >
         <Image src={camiseta1} width={520} height={480} alt='camiseta' />
 
         <footer>
