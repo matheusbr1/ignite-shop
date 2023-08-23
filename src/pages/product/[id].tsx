102dbf62ddb70791012a3d53704f4d49cc1dc370
Matheus Baron <matheusbaron10@gmail.com>
2023-08-22 22:09:00 -0300

    feat: create product page components struture and style

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,12 +1,24 @@
 import { } from 'next'
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  console.log(query)
-
   return (
-    <div>product</div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 78,90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A illo dolorem eos aliquam corrupti praesentium consectetur quam. Ducimus autem expedita aut blanditiis temporibus deserunt optio laborum sunt. Dolore, odio blanditiis.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,8 @@ import { globalCss } from "@stitches/react";
 export const GlobalStyles = globalCss({
   '*': {
     margin: 0,
-    padding: 0
+    padding: 0,
+    boxSizing: 'border-box'
   },
   body: {
     '-webkit-font-smoothing': 'antialised',
