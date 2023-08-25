09c8911349507563b711d8c31003b7c07b1eece6
Matheus Baron <matheusbaron10@gmail.com>
2023-08-24 21:14:05 -0300

    feat: create success page components struture and style

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+import Link from 'next/link'
 
 export default function Success() {
   return (
-    <div>success</div>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhhul, <strong>Matheus Baron</strong>, sua <strong>camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href='/' >
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
